5c53109085cd35aa2ad35df464da22d0354ab629
Juan Carlos Gutierrez <gutierrez.8006@gmail.com>
2020-04-01 23:00:18 -0500

    [ADD] mongoose-autopopulate dependency added

---
+++ src/models/user.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const { compareSync, hashSync, genSaltSync } = require('bcryptjs');
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    name: { type: String, required: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true }
+});
+
+UserSchema.methods.toJson = function() {
+    let user = this.toObject(); // convierte a un objeto javascript
+    delete user.password;
+    return user;
+};
+
+UserSchema.methods.comparePassword = function(password) {
+    return compareSync(password, this.password);
+};
+
+UserSchema.pre('save', async function(next) {
+    const user = this;
+
+    if (!user.isModified('password')) { // si no se esta modificando
+        return next();
+    }
+
+    const salt = genSaltSync(10);
+    const hashedPassword = hashSync(user.password, salt);
+    user.password = hashedPassword;
+    next();
+});
+
+module.exports = mongoose.model('user', UserSchema);
